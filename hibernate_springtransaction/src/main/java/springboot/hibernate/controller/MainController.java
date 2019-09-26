397132f2e69d392ef2c9b2209256eead4def98bc
Thanh Chu <cvthanh@tma.com.vn>
2019-09-26 18:36:36 +0700

    Update code

--- hibernate_springtransaction/src/main/java/springboot/hibernate/controller/MainController.java
+++ hibernate_springtransaction/src/main/java/springboot/hibernate/controller/MainController.java
@@ -6,14 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import springboot.hibernate.dao.BankAccountDAO;
 import springboot.hibernate.entity.Candidate;
 import springboot.hibernate.exception.BankTransactionException;
 import springboot.hibernate.form.SendMoneyForm;
 import springboot.hibernate.service.CandidateService;
+
+import javax.validation.Valid;
 
 @Controller
 public class MainController {
@@ -38,17 +43,35 @@ public class MainController {
         return "candidateDetail";
     }
 
+    @RequestMapping(value = "/candidateDetail", method = RequestMethod.POST)
+    public String edit(@ModelAttribute(value = "candidate") @Valid Candidate candidate, BindingResult bindingResult,
+                       final RedirectAttributes redirectAttributes) {
+//		CandidateValidator candidateValidator = new CandidateValidator();
+//		candidateValidator.validate(candidate, bindingResult);
+//        if (bindingResult.hasErrors()) {
+//            return "candidateDetail";
+//        } else {
+            // Authentication auth =
+            // SecurityContextHolder.getContext().getAuthentication();
+            // String name = auth.getName();
+            // Manager m = this.managerService.findManagerbyUserName(name);
+            // candidate.setManager(m);
+            candidateService.edit(candidate);
+            redirectAttributes.addFlashAttribute("message", "Save Applicant Successful");
+            return "redirect:/";
+//        }
+    }
+
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public String delete(@PathVariable(value = "id") int id) {
+    public String delete(@PathVariable(value = "id") int id, final RedirectAttributes redirectAttributes) {
         // Interview interview = new Interview();
         if (candidateService.find(id) == null) {
             return "redirect:/403.html";
         } else {
             candidateService.delete(id);
+            redirectAttributes.addFlashAttribute("message", "Delete Applicant Successful");
         }
-
-//        return "redirect:/candidatePage.html";
-        return "candidatePage";
+        return "redirect:/";
     }
 
     @RequestMapping(value = "/sendMoney", method = RequestMethod.GET)
